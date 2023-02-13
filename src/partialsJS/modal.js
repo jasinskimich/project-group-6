9e268f2a831e745d5798361f219ffa55ea19d52b
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2023-02-13 18:43:04 +0100

    Fix

--- src/partialsJS/modal.js
+++ src/partialsJS/modal.js
@@ -1,14 +1,10 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+const openModalBtn = document.querySelector('[data-modal-open]');
+const closeModalBtn = document.querySelector('[data-modal-close]');
+const modal = document.querySelector('[data-modal]');
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+openModalBtn.addEventListener('click', toggleModal);
+closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+function toggleModal() {
+  modal.classList.toggle('is-hidden');
+}
