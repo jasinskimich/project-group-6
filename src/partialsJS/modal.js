cc460158ade88d03978f495d59f4d5293b003f96
hnejman <108231655+hnejman@users.noreply.github.com>
2023-02-16 20:12:15 +0100

    update

--- src/partialsJS/modal.js
+++ src/partialsJS/modal.js
@@ -4,8 +4,65 @@ const box = document.querySelector('.box');
 const closeModalBtn = document.querySelector('.modal__btn-close');
 const modal = document.querySelector('[data-modal]');
 
+localStorage.setItem("queue", "");
+localStorage.setItem("watched", "")
+
 export function attachModal() {
   box.addEventListener('click', toggleModalOn);
+}
+
+async function refershModal(id){
+  const url ='https://api.themoviedb.org/3/'
+  const apiKey ='b32896ed8f56a3105cdf45e097423bca'
+  await fetch(
+    `${url}movie/${id}?api_key=${apiKey}`
+  )
+  .then(r => r.json())
+  .then(r => {
+    console.log(r);
+    const details = modal.children[0].children[2];
+    const description = details.children[1].children[1];
+    modal.children[0].children[1].src = `https://image.tmdb.org/t/p/w500${r.poster_path}`;
+    modal.children[0].children[1].alt = r.original_title;
+    details.children[0].textContent = r.title;
+    description.children[0].children[0].firstChild.textContent = r.vote_average; 
+    description.children[0].children[1].firstChild.textContent = r.vote_count;
+    description.children[1].textContent = r.popularity;
+    description.children[2].textContent = r.original_title;
+    let content = '';
+    r.genres.forEach(element => {
+      content += element.name + ', ';
+    });
+    description.children[3].textContent = content.slice(0, -2);
+    details.children[2].children[1].textContent = r.overview;
+    const addToWatched = document.querySelector(".modal__btn--watched");
+    const addToQueue = document.querySelector(".modal__btn--queue");
+    addToWatched.addEventListener("click", ()=>{
+      let watched = localStorage.getItem("watched");
+      let check = watched.split(";");
+      if(check.includes(id)){
+        return 0;
+      }else{
+        watched = watched + id + ';';
+        localStorage.removeItem("watched");
+        localStorage.setItem("watched", watched);
+      }
+    });
+    addToQueue.addEventListener("click", ()=>{
+      let queue = localStorage.getItem("queue");
+      let check = queue.split(";");
+      if(check.includes(id)){
+        return 0;
+      }else{
+        queue = queue + id + ';';
+        localStorage.removeItem("queue");
+        localStorage.setItem("queue", queue);
+      }
+    });
+  })
+  .catch(error => {
+    console.log('error: ' + error);}
+    );
 }
 
 function toggleModalOn(event) {
@@ -16,7 +73,7 @@ function toggleModalOn(event) {
   }
   closeModalBtn.addEventListener('click', toggleModalOff);
 
-  console.log(movie.children[1].textContent);
+  refershModal(movie.children[1].textContent)
 }
 
 function toggleModalOff() {
