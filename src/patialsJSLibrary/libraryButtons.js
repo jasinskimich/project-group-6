9d5ab1ee75e171c6db3ca79149544bd3430ead3e
Matjelko <matjelko11@gmail.com>
2023-02-20 20:20:52 +0100

    update-photo

--- src/patialsJSLibrary/libraryButtons.js
+++ src/patialsJSLibrary/libraryButtons.js
@@ -5,9 +5,6 @@ const buttonUnclicked = document.querySelector('.options__button');
 const buttonWatched = document.querySelector('#options__button--watched');
 const buttonQueue = document.querySelector('#options__button--queue');
 
-// const buttonAddToWatched = document.querySelector('#id or .class');
-// const buttonAddToQueue = document.querySelector('#id or .class');
-
 buttonUnclicked.addEventListener('click', () => {
   buttonUnclicked.classList.remove('options__button');
   buttonUnclicked.classList.add('options__button--clicked');
@@ -23,21 +20,3 @@ buttonClicked.addEventListener('click', () => {
   buttonUnclicked.classList.add('options__button');
   
 });
-
-// buttonAddToWatched.addEventListener('click', () => {
-// 	localStorage.setItem('Watched', ' ');
-// });
-
-// buttonAddToQueue.addEventListener('click', () => {
-// 	localStorage.setItem('Queue', ' ');
-// });
-
-// buttonWatched.addEventListener('click', () => {
-//   const filmWatched = localStorage.getItem('Watched');
-//   console.log(filmWatched);
-// });
-
-// buttonQueue.addEventListener('click', () => {
-//   const filmQueue = localStorage.getItem('Queue');
-//   console.log(filmQueue);
-// });
